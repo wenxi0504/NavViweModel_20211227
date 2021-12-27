c2b6ea61c81b320021b32ae3842fc62e759a4535
Sophie <wenxi1988@hotmail.com>
2021-12-27 17:12:48 -0500

    add variable,getNumber and add method

--- app/src/main/java/com/example/navviewmodel_20211227/MyViewModel.java
+++ app/src/main/java/com/example/navviewmodel_20211227/MyViewModel.java
@@ -1,4 +1,33 @@
 package com.example.navviewmodel_20211227;
 
-public class MyViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel
+{   // variable
+    private MutableLiveData<Integer> number;
+
+    //getNumber method
+    public MutableLiveData<Integer> getNumber()
+    {
+        if (number==null)
+        {
+            MutableLiveData<Integer> number = new MutableLiveData<>();
+            number.setValue(0);
+        }
+        return number;
+    }
+
+    // add method
+    void add(int x)
+    {
+        number.setValue(number.getValue()+x);
+        if (number.getValue()<0)
+        {
+            number.setValue(0);
+        }
+
+    }
+
+
 }
