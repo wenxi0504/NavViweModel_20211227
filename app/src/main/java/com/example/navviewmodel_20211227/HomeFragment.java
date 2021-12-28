7d3e43c9eed1dfb2dd71ddf7bf9f55f7bc04b370
Sophie <wenxi1988@hotmail.com>
2021-12-27 20:32:55 -0500

    binding data

--- app/src/main/java/com/example/navviewmodel_20211227/HomeFragment.java
+++ app/src/main/java/com/example/navviewmodel_20211227/HomeFragment.java
@@ -1,12 +1,19 @@
 package com.example.navviewmodel_20211227;
 
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.navviewmodel_20211227.databinding.FragmentHomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +66,26 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+
+        MyViewModel myViewModel;
+        // similar like new key word, ViewModelProviders is to provide correct viewModel
+        myViewModel = new ViewModelProvider(getActivity()).get(MyViewModel.class);
+        FragmentHomeBinding binding;
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(getActivity());
+
+        // enter button to navigate from home page to detail page
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // set navigation controller
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_homeFragment_to_detailFragment);
+            }
+        });
+        //view 类型的根节点
+        return binding.getRoot();
     }
 }
